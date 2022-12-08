f1dbdaa55b7e3c7f1473a3855cc07a3a9d8bb6a0
hisho <hisho.web@gmail.com>
2022-12-09 01:53:04 +0900

    ✨ postページ用のqueryを作成しPostコンポーネントからページQueryに差し替え

--- frontend/app/posts/page.tsx
+++ frontend/app/posts/page.tsx
@@ -1,28 +1,38 @@
-import { Posts } from '@src/feature/post/posts/Posts'
 import { PostCard } from '@src/feature/post/PostCard/PostCard'
 import Link from 'next/link'
+import { GraphQLClient } from 'graphql-request'
+import { PostsPageQueryDocument } from '@app/posts/postsPage.generated'
+import { use } from 'react'
+import { NEXT_PUBLIC_GRAPHQL_URL } from '@src/constant/env'
+
+const fetchPostsPageQuery = async () => {
+  /**
+   * TODO: new GraphQLClientを切り出す
+   */
+  return await new GraphQLClient(NEXT_PUBLIC_GRAPHQL_URL)
+    .request(PostsPageQueryDocument)
+    .then((data) => data)
+}
 
 const Page = () => {
+  const { posts } = use(fetchPostsPageQuery())
+
   return (
     <div className={'py-10'}>
       <h1 className={'text-2xl font-bold'}>投稿</h1>
       <div className={'h-4'} aria-hidden />
-      <Posts>
-        {(posts) => (
-          <ul className={'grid grid-cols-2 gap-x-2 gap-y-4'}>
-            {posts.map((post) => (
-              <li key={`Posts_${post.id}`}>
-                <Link
-                  href={`/posts/${post.id}`}
-                  className={'transition-opacity hover:opacity-75'}
-                >
-                  <PostCard post={post} />
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </Posts>
+      <ul className={'grid grid-cols-2 gap-x-2 gap-y-4'}>
+        {posts.map((post) => (
+          <li key={`Posts_${post.id}`}>
+            <Link
+              href={`/posts/${post.id}`}
+              className={'transition-opacity hover:opacity-75'}
+            >
+              <PostCard post={post} />
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
