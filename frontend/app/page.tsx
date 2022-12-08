f6611dfb115668e4c94f515b52aa584dea0fc421
hisho <hisho.web@gmail.com>
2022-12-09 01:25:47 +0900

    🔥 topページからpostsの取得を削除

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,20 +1,5 @@
-import { Posts } from '@src/feature/post/posts/Posts'
-
 const Page = () => {
-  return (
-    <div>
-      <Posts>
-        {(posts) =>
-          posts.map(({ title, id }) => (
-            <div key={`Posts_${id}`}>
-              <p>{id}</p>
-              <h1>{title}</h1>
-            </div>
-          ))
-        }
-      </Posts>
-    </div>
-  )
+  return <>main</>
 }
 
 export default Page
