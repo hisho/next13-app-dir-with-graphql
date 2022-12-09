f2c66faa5131c882387f7ec67b471225d1463693
hisho <hisho.web@gmail.com>
2022-12-09 19:13:26 +0900

    👽 postに紐づくtagのnullを削除

--- backend/src/module/post/entities/post.entity.ts
+++ backend/src/module/post/entities/post.entity.ts
@@ -1,5 +1,6 @@
-import { ObjectType, PickType } from '@nestjs/graphql';
+import { Field, ObjectType, PickType } from '@nestjs/graphql';
 import { Post as PostModel } from '../../../@generated/prisma-nestjs-graphql/post/post.model';
+import { Tag } from '../../../@generated/prisma-nestjs-graphql/tag/tag.model';
 
 @ObjectType()
 export class Post extends PickType(PostModel, [
@@ -8,5 +9,13 @@ export class Post extends PickType(PostModel, [
   'content',
   'createdAt',
   'updatedAt',
-  'tags',
-]) {}
+]) {
+  /**
+   * Postに紐付いているタグ
+   */
+  @Field(() => [Tag], {
+    nullable: false,
+    description: 'Postに紐付いているタグ',
+  })
+  tags: Tag[];
+}
