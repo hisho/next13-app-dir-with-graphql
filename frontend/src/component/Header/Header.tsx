3e84076aa969f4340e65312fbc9199f3795658e1
hisho <hisho.web@gmail.com>
2022-12-12 10:45:09 +0900

    ✨ Headerにpost作成ページのリンクとTOPへのリンクを追加

--- frontend/src/component/Header/Header.tsx
+++ frontend/src/component/Header/Header.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+
 export const Header = () => {
   return (
     <header className={'bg-base-200'}>
-      <div className={'py-6 w-full max-w-3xl px-4 mx-auto'}>header</div>
+      <div className={'py-6 w-full max-w-3xl px-4 mx-auto'}>
+        <div className={'flex items-center'}>
+          <Link href={'/'} className={'link link-hover text-2xl uppercase'}>
+            header
+          </Link>
+          <div aria-hidden className={'flex-1'} />
+          <Link href={'/user/post/new'} className={'btn btn-primary'}>
+            投稿
+          </Link>
+        </div>
+      </div>
     </header>
   )
 }
