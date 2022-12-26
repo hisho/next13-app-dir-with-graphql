094ed2a08bbec7b955ecbf8f99acd169bc6f8058
hisho <hisho.web@gmail.com>
2022-12-26 09:04:25 +0900

    ⚰️ 不要になったコードの削除

--- frontend/app/user/posts/page.tsx
+++ frontend/app/user/posts/page.tsx
@@ -1,32 +1,27 @@
 import { PostCard } from '@src/feature/post/PostCard/PostCard'
-import { createGraphQLClient } from '@src/util/createGraphQLClient/createGraphQLClient'
-import { UserPostsPageQueryDocument } from '@app/user/posts/userPostsPage.generated'
 import { Link } from '@src/component/Link/Link'
-
-const fetchPostsPageQuery = () => {
-  return createGraphQLClient()
-    .request(UserPostsPageQueryDocument)
-    .then((data) => data)
-}
+import { PostCardList } from '@src/feature/post/PostCardList/PostCardList'
 
 const Page = async () => {
-  const { posts } = await fetchPostsPageQuery()
-
   return (
     <div className={'py-10'}>
       <h1 className={'text-2xl font-bold'}>投稿</h1>
       <div className={'h-4'} aria-hidden />
       <ul className={'grid grid-cols-2 gap-x-2 gap-y-4'}>
-        {posts.map((post) => (
-          <li key={`Posts_${post.id}`}>
-            <Link
-              href={(path) => path.user.posts.edit._postId(post.id).$url()}
-              className={'transition-opacity hover:opacity-75'}
-            >
-              <PostCard post={post} />
-            </Link>
-          </li>
-        ))}
+        <PostCardList>
+          {({ posts }) =>
+            posts.map((post) => (
+              <li key={`Posts_${post.id}`}>
+                <Link
+                  href={(path) => path.user.posts.edit._postId(post.id).$url()}
+                  className={'transition-opacity hover:opacity-75'}
+                >
+                  <PostCard post={post} />
+                </Link>
+              </li>
+            ))
+          }
+        </PostCardList>
       </ul>
     </div>
   )
